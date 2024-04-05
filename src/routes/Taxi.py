c25456889c875b35bbd3a475bac961e2e38acc5e
Korin <kav02@outlook.com>
2024-04-05 10:07:37 -0600

    Listar todos los taxis

--- src/routes/Taxi.py
+++ src/routes/Taxi.py
@@ -1,8 +1,21 @@
 from flask import Blueprint, jsonify
+from models.TaxiModel import TaxiModel
 
-main = Blueprint("taxi_blueprint", __name__)
+main_bp = Blueprint("main_bp", __name__)
 
 
-@main.route('/')
-def get_taxies():
-    return jsonify({'mensaje': 'Prueba'})
+@main_bp.route('/')
+def index():
+    return jsonify({'mensaje': 'Pagina principal'})
+
+
+taxi_bp = Blueprint("taxi_bp", __name__)
+
+
+@taxi_bp.route('/taxi')
+def get_taxies_list():
+    try:
+        taxies_list = TaxiModel.get_taxi_as_json()
+        return jsonify(taxies_list), 200
+    except Exception as ex:
+        return jsonify({'message': str(ex)}), 500

9fc07fd1fdc577473676169001612e3b78a18bfa
Aylin <aylin_santacruz@hotmail.com>
2024-04-09 18:55:16 -0500

    integrate with express

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express, { Application, Request, Response  } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerSetup from './docs/swagger';
 import taxiRouter from './routes/taxis';
 const app: Application = express();
 const PORT: number = 3000;
@@ -7,6 +9,7 @@ app.use(express.json());
 
 
 app.use('/', taxiRouter);
+app.use("/documentation", swaggerUi.serve,swaggerUi.setup(swaggerSetup))
 app.use('/', (req: Request, res: Response): void => {
     res.send('Hello world!');
 });

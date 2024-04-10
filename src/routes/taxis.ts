621298f3009ba23d7346d8799c5421a2d82a1158
Aylin <aylin_santacruz@hotmail.com>
2024-04-09 19:33:30 -0500

    feat(taxis.ts):Add documentation to get

--- src/routes/taxis.ts
+++ src/routes/taxis.ts
@@ -3,6 +3,27 @@ import { TaxisController } from '../controller/taxis';
 
 const router: Router = Router();
 
+/**
+ * Post track
+ * @openapi
+ * /taxis:
+ *    get:
+ *      tags:
+ *        - taxis
+ *      summary: "Obtener taxis"
+ *      description: Este endpoint es para obtener los taxis  
+ *      responses:
+ *        '200':
+ *          description: Operación exitosa.
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: array
+ *                      items:
+ *                          $ref: '#/components/schemas/Taxis'
+ */
+
+
 router.get('/', TaxisController.getAllTaxis);
 
 export default router;

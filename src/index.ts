83dd9de63ed28316c9cfb4cffcddbdb9e0070314
Aylin <aylin_santacruz@hotmail.com>
2024-04-14 22:14:39 -0500

    chore: Use main router in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
-import express, { Application, Request, Response  } from 'express';
-import taxiRouter from './routes/taxis';
+import express, { Application, Request, Response } from 'express';
+// import taxiRouter from './routes/taxis';
+import router from './routes';
 const app: Application = express();
 const PORT: number = 3000;
 
 app.use(express.json());
 
 
-app.use('/', taxiRouter);
+// app.use('/', taxiRouter);
+app.use(router);
+
 app.use('/', (req: Request, res: Response): void => {
     res.send('Hello world!');
 });

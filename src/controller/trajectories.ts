30cb1b39bb30743df5047880dc0990c122d2cb48
Aylin <aylin_santacruz@hotmail.com>
2024-04-14 22:25:03 -0500

    feat(trajectories.ts): Add pagination to the trajectories list

--- src/controller/trajectories.ts
+++ src/controller/trajectories.ts
@@ -4,7 +4,11 @@ import prisma from "../db";
 export const TrajectoriesController = {
     getAllTrajectories: async (req: Request, res: Response) =>{
         try {
-            const trajectories = await prisma.trajectories.findMany();
+            const { skip, take } = req.query;
+            const trajectories = await prisma.trajectories.findMany({
+                skip: skip ? Number(skip) : undefined,
+                take: take ? Number(take) : undefined,
+            });
             return res.status(200).json(trajectories);
         } catch (error:any) {
             return res.status(500).json({ message: error.message })

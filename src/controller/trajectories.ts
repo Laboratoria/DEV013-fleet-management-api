d41567fc68a3ef293a9f06f0e0380693f5c13190
Aylin <aylin_santacruz@hotmail.com>
2024-04-14 22:11:47 -0500

    feat(trajectories.ts):Add enppoint of getAllTrajectories

--- src/controller/trajectories.ts
+++ src/controller/trajectories.ts
@@ -2,5 +2,12 @@ import type { Request, Response } from "express";
 import prisma from "../db";
 
 export const TrajectoriesController = {
-    
+    getAllTrajectories: async (req: Request, res: Response) =>{
+        try {
+            const trajectories = await prisma.trajectories.findMany();
+            return res.status(200).json(trajectories);
+        } catch (error:any) {
+            return res.status(500).json({ message: error.message })
+        }
+    },
 }

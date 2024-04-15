aa1100b3280c10534aa5c045c137b71bb96ccba2
Aylin <aylin_santacruz@hotmail.com>
2024-04-15 09:34:14 -0500

    feat(trajectorires.js):Add router to trajectories

---
+++ src/routes/trajectories.ts
@@ -0,0 +1,8 @@
+import express,{ Router } from 'express';
+import { TrajectoriesController } from '../controller/trajectories';
+
+const router: Router = express.Router();
+
+router.get('/trajectories', TrajectoriesController.getAllTrajectories);
+
+export default router;

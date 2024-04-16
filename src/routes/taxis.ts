9d54d8a9067a0cdb5fab0bbaa52c73ea41b13682
Aylin <aylin_santacruz@hotmail.com>
2024-04-16 09:45:30 -0500

    chore: delete getLocation router

--- src/routes/taxis.ts
+++ src/routes/taxis.ts
@@ -4,7 +4,7 @@ import { TaxisController } from '../controller/taxis';
 const router: Router = express.Router();
 
 router.get('/taxis', TaxisController.getAllTaxis);
-router.get('/location',TaxisController.getLocationHistory);
+// router.get('/location',TaxisController.getLocationHistory);
 router.get('/lastLocation',TaxisController.getLastLocation)
 router.get('/taxis/:id',TaxisController.getTaxiById);
 router.post('/taxis',TaxisController.postTaxi);

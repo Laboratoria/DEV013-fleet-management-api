2e5303d89f79b9ec2c13819c4a483348db98675e
Aylin <aylin_santacruz@hotmail.com>
2024-04-17 10:30:51 -0500

    feat:change router

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import express, { Application, Request, Response  } from 'express';
 import swaggerUi from 'swagger-ui-express';
 import swaggerSetup from './docs/swagger';
-import taxiRouter from './routes/taxis';
+// import taxiRouter from './routes/taxis';
+import router from './routes';
 const app: Application = express();
 const PORT: number = 3000;
 
 app.use(express.json());
 
+app.use(router);
 
-app.use('/', taxiRouter);
 app.use("/documentation", swaggerUi.serve,swaggerUi.setup(swaggerSetup))
 app.use('/', (req: Request, res: Response): void => {
     res.send('Hello world!');

39432935415fe395d38ff7088ebaadd4e07b1a94
Aylin <aylin_santacruz@hotmail.com>
2024-04-17 10:31:22 -0500

    feat:Add documentation of trajectories get

--- src/routes/trajectories.ts
+++ src/routes/trajectories.ts
@@ -3,6 +3,41 @@ import { TrajectoriesController } from '../controller/trajectories';
 
 const router: Router = express.Router();
 
+/**
+ * Get track
+ * @openapi
+ * /trajectories:
+ *    get:
+ *      tags:
+ *        - Trajectories
+ *      summary: "Obtener todas las trayectorias"
+ *      description: Este endpoint es para obtener trayectorias
+ *      parameters:
+ *        - in: query
+ *          name: skip
+ *          schema:
+ *            type: integer
+ *            minimum: 1
+ *          description: Número de página que se desea obtener
+ *        - in: query
+ *          name: take
+ *          schema:
+ *            type: integer
+ *            minimum: 1
+ *            maximum: 100
+ *            default: 10
+ *          description: Número máximo de resultados por página
+ *      responses:
+ *        '200':
+ *          description: Operación exitosa.
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: array
+ *                      items:
+ *                          $ref: '#/components/schemas/Trajectories'
+ */
+
 router.get('/trajectories', TrajectoriesController.getAllTrajectories);
 router.get('/count',TrajectoriesController.getTrajectoriesCount);
 router.get('/trajectories/:id', TrajectoriesController.getTrajectoriesById);

5471bfd5144835b991a30609b4bae54b1f8eabdb
Aylin <aylin_santacruz@hotmail.com>
2024-04-17 11:34:37 -0500

    feat: Add getLocation router

--- src/routes/trajectories.ts
+++ src/routes/trajectories.ts
@@ -6,6 +6,7 @@ const router: Router = express.Router();
 router.get('/trajectories', TrajectoriesController.getAllTrajectories);
 router.get('/count',TrajectoriesController.getTrajectoriesCount);
 router.get('/trajectories/:id', TrajectoriesController.getTrajectoriesById);
+router.get('/location/:id',TrajectoriesController.getLocationHistory)
 router.post('/trajectories',TrajectoriesController.postTrajectories);
 router.put('/trajectories/:id', TrajectoriesController.putTrajectoryById);
 router.delete('/trajectories/:id',TrajectoriesController.deleteTrajectoriesById)

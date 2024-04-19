a38251cdadced219cd9f963c20bcd379cfb6a24c
Elizabeth <elizabethepj@gmail.com>
2024-04-19 12:29:05 +0200

    connect postgress

---
+++ .venv/src/conection_postgres.py
@@ -0,0 +1,18 @@
+"""Database connection"""
+import os
+import psycopg2
+
+
+try:
+    connection = psycopg2.connect(
+        host = os.getenv('POSTGRES_HOST'),
+        user = os.getenv('POSTGRES_USER'),
+        password = os.getenv('POSTGRES_PASSWORD'),
+        database = os.getenv('POSTGRES_DATABASE'),
+        port = 5432
+    )
+    print("Succefully connected")
+    cursor = connection.cursor()
+    cursor.execute("SELECT version()")
+except Exception as ex:
+    print(ex)

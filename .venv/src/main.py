2480b1547e75356e8d22ffbc532693ca61c1ba8a
Elizabeth <elizabethepj@gmail.com>
2024-04-24 17:42:18 +0200

    ambiente virtual

--- .venv/src/main.py
+++ .venv/src/main.py
@@ -1,35 +1,55 @@
-
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+#request servirá para configurar los parámetros
 from flask_sqlalchemy import SQLAlchemy
+from flasgger import Swagger
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://default:Hs1ODnhjr2lU@ep-holy-sunset-a405zt5y-pooler.us-east-1.aws.neon.tech/verceldb'
-
 db = SQLAlchemy(app)
 
-# Definir el modelo Taxi
+# Initialize Swagger
+swagger = Swagger(app)
+
+# Define the Taxi model
 class Taxi(db.Model):
+    """Model representing a Taxi."""
     __tablename__ = 'taxis'
     id = db.Column(db.Integer, primary_key=True)
-    plate = db.Column(db.String(20), nullable=False) 
+    plate = db.Column(db.String(20), nullable=False)
 
 @app.route("/")
 def root():
-    return "This is root"
+   return "This is root"
 
-# Ruta para mostrar todos los taxis
-# @app.route('/taxis')
-# def show_taxis():
-#     taxis = Taxi.query.all()
-#     return {'taxis': [{'id': taxi.id, 'plate': taxi.plate} for taxi in taxis]}
-
-@app.route('/taxis')
+# Route to show all taxis
+@app.route('/taxis', methods =['GET'])
 def show_taxis():
+    """
+    Endpoint to get all taxis.
+
+    ---
+    responses:
+      200 :
+        description: A JSON array of all taxis.
+        schema:
+          type: array
+          items:
+            type: object
+            properties:
+              id:
+                type: integer
+                description: The taxi ID.
+              plate:
+                type: string
+                description: Plate of the taxi.
+    """
     taxis = Taxi.query.all()
-    taxis_list = [{'id': taxi.id, 'plate': taxi.plate} for taxi in taxis]
-    return jsonify({'taxis': taxis_list})
+    return jsonify([{'id': taxi.id, 'plate': taxi.plate} for taxi in taxis])
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
 
 

52196d7280a5a9d5b46eddc5bf166eb25a0bc1b3
Aylin <aylin_santacruz@hotmail.com>
2024-04-25 11:33:41 -0500

    feat: update and delete test finally

--- test/mock.spec.ts
+++ test/mock.spec.ts
@@ -1,14 +1,16 @@
-import {allTaxisService,createTaxiService,taxiByIdService} from '../src/services/taxis';
+import { allTaxisService, createTaxiService, taxiByIdService, updateTaxiService,deleteTaxiService } from '../src/services/taxis';
 
-jest.mock('@prisma/client',() =>{
+jest.mock('@prisma/client', () => {
     const mockPrisma = {
-        taxis:{
-            findMany:jest.fn().mockResolvedValue([
-                {id:1,plate:'Taxi 1'},
-                {id:2,plate:'Taxi 2'}
+        taxis: {
+            findMany: jest.fn().mockResolvedValue([
+                { id: 1, plate: 'KKAA-0000' },
+                { id: 2, plate: 'BBHJ-0000' }
             ]),
-            findUnique: jest.fn().mockResolvedValue([{ id: 3, plate: 'Taxi 3' }]),
+            findUnique: jest.fn().mockResolvedValue([{ id: 3, plate: 'AAOP-0000' }]),
             create: jest.fn().mockResolvedValue({ id: 4, plate: 'CNCA-0000' }),
+            update: jest.fn().mockResolvedValue({ id: 4, plate: 'CNKO-0000' }),
+            delete:jest.fn().mockResolvedValue({ id: 4, plate: 'CNKO-0000' })
         },
     };
     return {
@@ -16,34 +18,34 @@ jest.mock('@prisma/client',() =>{
     }
 })
 
-describe('getAllTaxis',() =>{
-    it('return all taxis',async() =>{
+describe('getAllTaxis', () => {
+    it('return all taxis', async () => {
         const skip = 2;
         const take = 4;
 
-        const result = await allTaxisService(skip,take);
+        const result = await allTaxisService(skip, take);
 
         expect(result).toEqual([
-            { id: 1, plate: 'Taxi 1' },
-            { id: 2, plate: 'Taxi 2' }
+            { id: 1, plate: 'KKAA-0000' },
+            { id: 2, plate: 'BBHJ-0000' }
         ]);
 
     });
 });
 
-describe('getTaxiById',() =>{
-    it('return taxi by Id',async() =>{
+describe('getTaxiById', () => {
+    it('return taxi by Id', async () => {
         const taxiId = '3';
 
         const result = await taxiByIdService(taxiId);
 
         expect(result).toEqual([
-            { id: 3, plate: 'Taxi 3' }
+            { id: 3, plate: 'AAOP-0000' }
         ]);
 
     });
 
-    it('return null when an invalid taxiId is provided',async() =>{
+    it('return null when an invalid taxiId is provided', async () => {
         const taxiId = 'holacomoestas';
 
         const result = await taxiByIdService(taxiId);
@@ -54,19 +56,39 @@ describe('getTaxiById',() =>{
     });
 })
 
-describe('postTaxi',() =>{
-    it('create new taxi',async() =>{
+describe('postTaxi', () => {
+    it('create new taxi', async () => {
 
         const id = 4;
         const plate = 'CNCA-0000';
         const newTaxi = {
-            id:4,
-            plate:'CNCA-0000', 
+            id: 4,
+            plate: 'CNCA-0000',
         };
 
         const result = await createTaxiService(id, plate);
         console.log("🚀 ~ it ~ resul3t:", result)
 
         expect(result).toEqual(newTaxi);
-    })
+    });
+})
+
+describe('updateTaxi', () => {
+    it('Update a Taxi', async () => {
+        const taxiId = '4';
+        const updateData = {plate: "CNKO-0000"};
+        const result = await updateTaxiService(taxiId, updateData);
+        
+        expect(result).toEqual({ id: 4, plate: 'CNKO-0000' });
+    });
+})
+
+describe('deleteTaxi',() =>{
+    it('Delete a Taxi',async ()=>{
+        const taxiId='4';
+       const deletTaxi={ id: 4, plate: 'CNKO-0000' };
+       const response=await deleteTaxiService(taxiId);
+      //console.log("response",response);
+       expect(response).toEqual(deletTaxi);
+    });
 })

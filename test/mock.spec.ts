545127ceb046483f0678970201420170cd2f280e
Aylin <aylin_santacruz@hotmail.com>
2024-04-24 19:53:49 -0500

    feat: test to create a new taxi

--- test/mock.spec.ts
+++ test/mock.spec.ts
@@ -1,4 +1,4 @@
-import {AllTaxisService,TaxiByIdService} from '../src/services/taxis';
+import {allTaxisService,createTaxiService,taxiByIdService} from '../src/services/taxis';
 
 jest.mock('@prisma/client',() =>{
     const mockPrisma = {
@@ -7,7 +7,8 @@ jest.mock('@prisma/client',() =>{
                 {id:1,plate:'Taxi 1'},
                 {id:2,plate:'Taxi 2'}
             ]),
-            findUnique: jest.fn().mockResolvedValue([{ id: 3, plate: 'Taxi 3' }])
+            findUnique: jest.fn().mockResolvedValue([{ id: 3, plate: 'Taxi 3' }]),
+            create: jest.fn().mockResolvedValue({ id: 4, plate: 'CNCA-0000' }),
         },
     };
     return {
@@ -20,7 +21,7 @@ describe('getAllTaxis',() =>{
         const skip = 2;
         const take = 4;
 
-        const result = await AllTaxisService(skip,take);
+        const result = await allTaxisService(skip,take);
 
         expect(result).toEqual([
             { id: 1, plate: 'Taxi 1' },
@@ -29,11 +30,12 @@ describe('getAllTaxis',() =>{
 
     });
 });
+
 describe('getTaxiById',() =>{
     it('return taxi by Id',async() =>{
         const taxiId = '3';
 
-        const result = await TaxiByIdService(taxiId);
+        const result = await taxiByIdService(taxiId);
 
         expect(result).toEqual([
             { id: 3, plate: 'Taxi 3' }
@@ -44,10 +46,27 @@ describe('getTaxiById',() =>{
     it('return null when an invalid taxiId is provided',async() =>{
         const taxiId = 'holacomoestas';
 
-        const result = await TaxiByIdService(taxiId);
+        const result = await taxiByIdService(taxiId);
         console.log("🚀 ~ it ~ result:", result)
 
         expect(result).toBeNull();
 
     });
+})
+
+describe('postTaxi',() =>{
+    it('create new taxi',async() =>{
+
+        const id = 4;
+        const plate = 'CNCA-0000';
+        const newTaxi = {
+            id:4,
+            plate:'CNCA-0000', 
+        };
+
+        const result = await createTaxiService(id, plate);
+        console.log("🚀 ~ it ~ resul3t:", result)
+
+        expect(result).toEqual(newTaxi);
+    })
 })

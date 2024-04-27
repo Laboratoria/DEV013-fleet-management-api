1ad5508331c85b3b9e749009117fe3f0b8b13437
Aylin <aylin_santacruz@hotmail.com>
2024-04-26 23:11:49 -0500

    feat:Add id servcie search

--- src/services/trajectories.ts
+++ src/services/trajectories.ts
@@ -52,6 +52,11 @@ export const lastLocationService= async(skip:number, take:number): Promise<any>=
     return lastLocation;
 }
 
+export const trajectoryByIdService = async(id: number):Promise<any>=>{
+    const trajectory = await prisma.trajectories.findUnique({ where: { id: id } });
+    return  trajectory;
+}
+
 export interface ITrajectory {
     id?: number
     taxiId?: number

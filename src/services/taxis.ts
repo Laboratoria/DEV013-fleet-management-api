874ce3f822eec619419ec716f2d7c12968396702
Aylin <aylin_santacruz@hotmail.com>
2024-04-27 01:00:39 -0500

    fix:delete try catch

--- src/services/taxis.ts
+++ src/services/taxis.ts
@@ -9,56 +9,39 @@ export const allTaxisService = async (skip: number, take: number): Promise<ITaxi
 };
 
 export const taxiByIdService = async (id: number): Promise<any> => {
-    try {
-        const getId = await prisma.taxis.findUnique({
-            where: {
-                id: id
-            }
-        });
-        return getId;
-    } catch (error: any) {
-        throw new Error("Error al buscar el taxi en la base de datos");
-    }
+    const getId = await prisma.taxis.findUnique({
+        where: {
+            id: id
+        }
+    });
+    return getId;
 };
 
 export const createTaxiService = async (id: number, plate: string): Promise<any> => {
-    try {
-        if (id <= 0) {
-            throw new Error("El ID del taxi debe ser un número positivo");
-        }
-
-        const newTaxi = await prisma.taxis.create({
-            data: {
-                id, plate
-            }
-        });
-        return newTaxi;
-
-    } catch (error) {
-        throw new Error("Error al crear un nuevo taxi en la base de datos");
+    if (id <= 0) {
+        throw new Error("El ID del taxi debe ser un número positivo");
     }
+
+    const newTaxi = await prisma.taxis.create({
+        data: {
+            id, plate
+        }
+    });
+    return newTaxi;
 };
 
 export const updateTaxiService = async (id: number, update: any): Promise<any> => {
-    try {
-        const updateTaxi = await prisma.taxis.update({
-            where: { id: id },
-            data: update
-        });
-        return updateTaxi;
-    } catch (error) {
-        throw new Error("Error al actualizar el taxi en la base de datos");
-    }
+    const updateTaxi = await prisma.taxis.update({
+        where: { id: id },
+        data: update
+    });
+    return updateTaxi;
 };
 
 export const deleteTaxiService = async (id: string): Promise<any> => {
-    try {
-        const taxiId = parseInt(id);
-        const deleteTaxi = await prisma.taxis.delete({ where: { id: taxiId } });
-        return deleteTaxi;
-    } catch (error) {
-        throw new Error("Error al eliminar taxi en la base de datos");
-    }
+    const taxiId = parseInt(id);
+    const deleteTaxi = await prisma.taxis.delete({ where: { id: taxiId } });
+    return deleteTaxi;
 }
 
 export interface ITaxi {

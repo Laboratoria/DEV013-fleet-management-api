6b5d121375c8cad033eede25b165bbfa8972652f
Elizabeth <elizabethepj@gmail.com>
2024-04-29 10:16:23 +0200

    se elimina entorno de los commits

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+#from src.main import app
+
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    with app.test_client() as client:
+        with app.app_context():
+            yield client

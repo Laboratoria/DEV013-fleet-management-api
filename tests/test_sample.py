f26a09344723b07258628dcebe7f420be49d9e4f
Etelbina <etelbinacanedo@gmail.com>
2024-04-30 08:24:04 -0600

    tests endpoint 1

--- tests/test_sample.py
+++
@@ -1,5 +0,0 @@
-import pytest
-from testing_funct import func
-
-def test_answer():
-    assert func(3) == 5
--- tests/testing_funct.py
+++
@@ -1,5 +0,0 @@
-# content of test_sample.py
-def func(x):
-    return x + 1
-
-print(func(4))

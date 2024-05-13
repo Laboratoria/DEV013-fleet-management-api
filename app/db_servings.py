1d673bdbfd615236bf083335d751057233720660
Etelbina <etelbinacanedo@gmail.com>
2024-05-13 12:44:41 -0600

    optimizing dates request

--- app/db_servings.py
+++ app/db_servings.py
@@ -10,18 +10,13 @@ def get_taxi_from_db():
    crsr.execute(f"SELECT * FROM taxis ORDER BY id ASC LIMIT {limit} OFFSET {page};")
    return crsr.fetchall()
 
-# todo como le quito el between para pedir solo las de un solo dís como comparar el dia de un timestamp
 def get_trajectories_from_db():
    limit = request.args.get("limit", default=10)
    page = request.args.get("page", default=1)
    taxi = request.args.get("taxi_id", default=6418)
    date = request.args.get("date", default='2008-02-02')
-   date_format = datetime.strptime(date, '%Y-%m-%d').date()
-   next_day = date_format + timedelta(days=1)
-   print(date_format)
-   print(next_day)
    crsr = connection.cursor()
-   crsr.execute(f"SELECT * FROM trajectories WHERE taxi_id={taxi} AND date(date) BETWEEN '{date_format}' AND '{next_day}' ORDER BY taxi_id ASC LIMIT {limit} OFFSET {page};")
+   crsr.execute(f"SELECT * FROM trajectories WHERE taxi_id={taxi} AND CAST(date AS DATE) = '{date}' ORDER BY taxi_id ASC LIMIT {limit} OFFSET {page};")
    return crsr.fetchall()
 
 # el cliente solo pide la ruta
